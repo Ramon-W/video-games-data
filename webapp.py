209947666c5f03c7d24e65ce6846de2f2c93e66a
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-04-01 09:32:10 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,14 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json
 
+app = Flask(__name__)
+
+@app.route('/')
+def render_home():
+    return render_template('home.html')
+
+@app.route('/popular')
+def render_popular():
+    with open('broadway.json') as video_game:
+        game = json.load(video_game)
