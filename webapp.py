0ea57ebbfe2975267c5d34cc46bb1eac448646af
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-04-02 09:30:28 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,8 +7,3 @@ app = Flask(__name__)
 @app.route('/')
 def render_home():
     return render_template('home.html')
-
-@app.route('/popular')
-def render_popular():
-    with open('video_games.json') as video_game:
-        game = json.load(video_game)
